d6fc4ffcb65a99be70be624a9b1e9138bada07e2
马天 <13162378587@163.com>
2020-12-08 14:21:16 +0800

    ...

---
+++ Mateen/Classes/XD.h
@@ -0,0 +1,18 @@
+//
+//  XD.h
+//  Mateen
+//
+//  Created by mateen on 2020/12/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XD : NSObject
+
+- (void)xd;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Mateen/Classes/XD.m
@@ -0,0 +1,17 @@
+//
+//  XD.m
+//  Mateen
+//
+//  Created by mateen on 2020/12/8.
+//
+
+#import "XD.h"
+
+@implementation XD
+
+-(void)xd
+{
+    NSLog(@"12312321321xxxx");
+}
+
+@end
